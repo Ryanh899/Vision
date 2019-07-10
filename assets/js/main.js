8a8e745c6d49c61e6c9648a72c3a8f7cb8fc303a
Alex Hwong <violinmana@gmail.com>
2019-07-09 20:43:06 -0700

    fixed undefined errors, added clicked value to buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,39 +8,42 @@ var requestArr = [];
 function addRequests(picData) {
     this.request = {}
     this.request.requests = [{}]
-    this.request.requests[0].image = { "content": picData }
+    this.request.requests[0].image = {
+        "content": picData
+    }
     this.request.requests[0].features = [{
-        "type": "LABEL_DETECTION",
-        "maxResults": 1
-    },
-    {
-        "type": "FACE_DETECTION",
-        "maxResults": 10
-    },
-    {
-        "type": "OBJECT_LOCALIZATION",
-        "maxResults": 10
-    },
-    {
-        "type": "DOCUMENT_TEXT_DETECTION",
-        "maxResults": 10
-    },
-    {
-        "type": "LANDMARK_DETECTION",
-        "maxResults": 10
-    },
-    {
-        "type": "WEB_DETECTION",
-        "maxResults": 10
-    },
-    {
-        "type": "SAFE_SEARCH_DETECTION",
-        "maxResults": 10
-    },
-    {
-        "type": "IMAGE_PROPERTIES",
-        "maxResults": 10
-    },]
+            "type": "LABEL_DETECTION",
+            "maxResults": 1
+        },
+        {
+            "type": "FACE_DETECTION",
+            "maxResults": 10
+        },
+        {
+            "type": "OBJECT_LOCALIZATION",
+            "maxResults": 10
+        },
+        {
+            "type": "DOCUMENT_TEXT_DETECTION",
+            "maxResults": 10
+        },
+        {
+            "type": "LANDMARK_DETECTION",
+            "maxResults": 10
+        },
+        {
+            "type": "WEB_DETECTION",
+            "maxResults": 10
+        },
+        {
+            "type": "SAFE_SEARCH_DETECTION",
+            "maxResults": 10
+        },
+        {
+            "type": "IMAGE_PROPERTIES",
+            "maxResults": 10
+        },
+    ]
 }
 //pushes new requests to arr/ creates new request objs 
 function newRequest(img) {
@@ -74,62 +77,73 @@ $('#submit-button').on('click', function () {
             console.log(webArray)
 
             webArray.forEach(function (element) {
-                let newButton = (`<button class='btn newButton btn-primary mt-1 mb-1 ml-1 mr-1' value='${element.description}'> ${element.description}`)
 
+                if (element.description) {
+                    let newButton = (`<button class='btn newButton btn-primary mt-1 mb-1 ml-1 mr-1' clicked='false' value='${element.description}'> ${element.description}`)
 
-
-                $('#buttons').append(newButton)
-                console.log(element)
-
-
+                    $('#buttons').append(newButton)
+                    console.log(element)
+                }
             });
 
         })
     n++
 })
 
-
 $(document.body).on("click", ".newButton", function () {
 
+    if ($(this).attr('clicked') === 'false') {
 
-    // search term
-    var searchQuery = $(this).attr('value')
+        $(this).attr('clicked', 'true')
 
-    console.log(searchQuery)
+        // search term
+        var searchQuery = $(this).attr('value')
 
-    // key to api
-    var apiKey = '00c5bc8f-694b-401c-8e1a-3d53225e08f3';
+        console.log(searchQuery)
 
-    // search term with apikey
-    var apiRoute = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${searchQuery}?key=${apiKey}`;
-    $.get({
-        url: apiRoute
-    }).done(function (response) {
-        // check console for return obj
-        console.log(response);
+        // key to api
+        var apiKey = '00c5bc8f-694b-401c-8e1a-3d53225e08f3';
 
+        // search term with apikey
+        var apiRoute = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${searchQuery}?key=${apiKey}`;
+        $.get({
+            url: apiRoute
+        }).done(function (response) {
+            // check console for return obj
+            console.log(response);
 
-        let synonymArray = response[0].def[0].sseq[0][0][1].syn_list[0]
-//hashtag function 
+            if (!Array.isArray(response) || !response.length) {
+                let result = searchQuery;
+                let addHash = '#'
+                let hashTag = addHash.concat(result)
+                console.log(hashTag)
+                console.log(searchQuery)
+                $('.results').append(`<div> ${result}`)
+                $('.hashTag-results').append(`<div> ${hashTag}`)
+            } else if (!response[0].def) {
+                let result = searchQuery;
+                let addHash = '#'
+                let hashTag = addHash.concat(result)
+                console.log(hashTag)
+                console.log(searchQuery)
+                $('.results').append(`<div> ${result}`)
+                $('.hashTag-results').append(`<div> ${hashTag}`)
+            } else {
+                let synonymArray = response[0].def[0].sseq[0][0][1].syn_list[0]
+                //hashtag function 
 
-
-
-        synonymArray.forEach(function (element) {
-            let result = element.wd; 
-            let addHash = '#'
-            let hashTag = addHash.concat(result)
-            console.log(hashTag)
-            console.log(element.wd)
-            $('.results').append(`<div> ${result}`)
-            $('.hashTag-results').append(`<div> ${hashTag}`)
+                synonymArray.forEach(function (element) {
+                    let result = element.wd;
+                    let addHash = '#'
+                    let hashTag = addHash.concat(result)
+                    console.log(hashTag)
+                    console.log(element.wd)
+                    $('.results').append(`<div> ${result}`)
+                    $('.hashTag-results').append(`<div> ${hashTag}`)
+                })
+            }
         });
-    });
-
-    //next api call
-
-
-
-
+    }
 });
 
 // Tabs Transition 
@@ -154,4 +168,4 @@ function copyToClipboard(element) {
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
-  }
+}
